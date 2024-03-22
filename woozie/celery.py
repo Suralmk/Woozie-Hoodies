759f2e147d9cbcfc8ed685c2e04d9302dd94d1ab
Suralmk <surafelmelaku940@gmail.com>
2024-03-22 19:33:37 +0300

    gevent is installed to fix with the not succeding problem of celery

--- woozie/celery.py
+++ woozie/celery.py
@@ -1,7 +1,10 @@
+from __future__ import absolute_import, unicode_literals
+
 import os 
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'woozie.settings')
+
 
 celery_app = Celery('woozie')
 
