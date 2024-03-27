f55d58513c457ce6d9638cbee8c3d859a2068ca0
Suralmk <surafelmelaku940@gmail.com>
2024-03-27 20:43:53 +0300

    handeling payment for unpaid ones from order list page

--- payment/urls.py
+++ payment/urls.py
@@ -4,10 +4,9 @@ from . import webhooks
 app_name = 'payment'
 
 urlpatterns = [
-    path('process/', payment_process, name='process'),
+    path('process/<slug:order_id>/', payment_process, name='process'),
     path('completed/<slug:tx_ref>/', payment_completed, name='completed'),
     path('canceled/<slug:tx_ref>/', payment_canceled, name='canceled'),
     path('webhook', webhooks.chapa_webhook, name='webhook'),
-
     path("list/", payment_list, name="payment_list"),
 ]
